338d9e25171c63420be917024daa34f3a7d7ab34
ugesh-2603 <75109057+ugesh-2603@users.noreply.github.com>
2023-06-11 08:06:17 +0530

    Update BinarySwapNibbles.cs

--- Logical_Programming/BinarySwapNibbles.cs
+++ Logical_Programming/BinarySwapNibbles.cs
@@ -10,35 +10,36 @@ namespace Logical_Programming
     {
         public static string swapNibbles(int n)
         {
-            string binary = BinaryConversion.Binary(n);
-            while (binary.Length % 8 != 0)
+            string binary = BinaryConversion.Binary(n);  // converting 
+            while (binary.Length % 8 != 0)  // binary string with zeros to ensure its length is a multiple of 8
             {
                 binary = "0" + binary;
             }
 
-            string swappedBinary = "";
-            for (int i = 0; i < binary.Length; i += 8)
+            string swappedBinary = "";  // Createing a new string 
+            for (int i = 0; i < binary.Length; i += 8)  // // Iterate over the binary string in increments of 8
             {
-                swappedBinary += binary.Substring(i + 4, 4) + binary.Substring(i, 4);
+                swappedBinary += binary.Substring(i + 4, 4) + binary.Substring(i, 4); // The purpose of these lines is to swap the positions of the two nibbles within each 8-bit group in the binary string. By appending the second nibble first and then the first nibble, the positions of the nibbles are effectively swapped.
             }
 
-            return swappedBinary;
+            return swappedBinary; //  // Return the swapped binary rep
            
         }
 
         public static string toDeci(string swappedBinary)
         {
-            int decimalNum = 0;
-            for (int i = 0; i < swappedBinary.Length; i++)
+            int decimalNum = 0;  // // Initialize a variable to store the decimal value
+            for (int i = 0; i < swappedBinary.Length; i++)  // Iterate over each character in the swapped binary string
             {
-                if (swappedBinary[i] == '1')
+                if (swappedBinary[i] == '1') // If the current character is '1', update the decimal value
+                                             // by adding the corresponding power of 2 based on its position
                 {
                     decimalNum += (int)Math.Pow(2, swappedBinary.Length - i - 1);
                 }
             }
 
             
-            return Convert.ToString(decimalNum);
+            return Convert.ToString(decimalNum); // Convert the decimal value to a string and return it
         }
     }
 }
