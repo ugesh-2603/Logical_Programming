eb9b4d7650f15acda615c7a26e91b3b473c0746c
ugesh-2603 <75109057+ugesh-2603@users.noreply.github.com>
2023-06-11 08:18:19 +0530

    Update Vending_Machine.cs

--- Logical_Programming/Vending_Machine.cs
+++ Logical_Programming/Vending_Machine.cs
@@ -8,17 +8,18 @@ namespace Logical_Programming
 {
     internal class Vending_Machine
     {
-        static int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+        static int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };  // This line declares an array called notes of type int and initializes it with the denominations of currency notes in descending order.
 
         public static void Ven_mac(int amount)
         {
             CalculateChange(amount);
         }
 
-        public static void CalculateChange(int change)
+        public static void CalculateChange(int change) // This is the CalculateChange method that takes a change parameter. 
+          
         {
-            List<int> result = new List<int>();
-            int remaining = change;
+            List<int> result = new List<int>();  //It initializes an empty list called result to store the denominations of notes required. 
+            int remaining = change;  // It also initializes an int variable called remaining to keep track of the remaining change to be calculated.
 
             for (int i = 0; i < notes.Length; i++)
             {
